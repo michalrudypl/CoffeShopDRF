86e941f4b014c4d1bd34005c24794f12040e8952
Michal Rudawski <michalrudawskiit@gmail.com>
2022-11-16 12:29:40 +0100

    add login path

--- authx/viewsets.py
+++ authx/viewsets.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .serializers import UserSerializer
 
 User = get_user_model()
@@ -10,3 +10,4 @@ class UserViewSet(ModelViewSet):
 
     queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
