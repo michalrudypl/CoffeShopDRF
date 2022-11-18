1557820d860bffbb952d0115e028dc00b84a5822
Michal Rudawski <michalrudawskiit@gmail.com>
2022-11-18 21:04:46 +0100

    Add logout system

--- authx/viewsets.py
+++ authx/viewsets.py
@@ -1,4 +1,7 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, logout
+from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.response import Response
+from authx.permissions import IsOwnerUser
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .serializers import UserSerializer
@@ -10,4 +13,10 @@ class UserViewSet(ModelViewSet):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwnerUser]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        logout(request)
+        self.perform_destroy(instance)
+        return Response(status=HTTP_204_NO_CONTENT)
